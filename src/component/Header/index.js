9f88c97ba921fe7ab2dd055be2d7ea87d3d89686
Matheus Felix Ribeiro <matheusfelixr@hotmail.com>
2021-03-06 13:05:23 -0300

    color teste

--- src/component/Header/index.js
+++ src/component/Header/index.js
@@ -24,7 +24,7 @@ export default class Header extends Component {
 
     render() {
         return (
-            <HeaderNavBar bg="primary" variant="dark">
+            <HeaderNavBar bg="primary">
 
                 <HeaderContainerBrand>
                     <HeaderNavBar.Brand>
